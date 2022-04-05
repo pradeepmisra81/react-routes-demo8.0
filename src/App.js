2a9857d8d992aaf3d60b434b361d1f4e31c6ccd1
pradeep <pradeep.misra@gmail.com>
2022-04-05 13:52:08 +0530

    Implemented Routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-function App() {
-  return (
-    <div>
+import { Route, Switch } from "react-router-dom";
 
-    </div>
+import AllQuotes from "./pages/AllQuotes";
+import NewQuotes from "./pages/NewQuotes";
+import QuoteDetail from "./pages/QuoteDetail";
+
+const App = () => {
+  return (
+    <Switch>
+      <Route path='/quotes' exact>
+        <AllQuotes/>
+      </Route>
+      <Route path='/quotes/:quoteid'>
+        <QuoteDetail/>
+      </Route>
+      <Route path='new-quote'>
+        <NewQuotes/>
+      </Route>
+    </Switch>
   );
 }
 
---
+++ src/pages/NewQuotes.js
@@ -0,0 +1,5 @@
+const NewQuotes = () => {
+    return <h1>New Quote page</h1>
+}
+
+export default NewQuotes;
